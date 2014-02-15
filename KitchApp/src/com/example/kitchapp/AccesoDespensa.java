ec9b44394ddb3a3095a858d0d859b0015c63189a
vivianasandagorda <viviycarlos55@gmail.com>
2014-02-15 23:45:22 +0100

    Interfaz-añadirManualmente
    Implementación interfaz añadir productos a la despensa manualmente

---
+++ KitchApp/src/com/example/kitchapp/AccesoDespensa.java
@@ -0,0 +1,41 @@
+package com.example.kitchapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AccesoDespensa extends Activity implements OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_acceso_despensa);
+		Toast.makeText(this, "Actividad despensa", Toast.LENGTH_SHORT).show();
+		Button b=(Button)findViewById(R.id.button_add);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.acceso_despensa, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.button_add:
+			Intent i = new Intent(this,AddDespensa.class);
+			startActivity(i);
+			break;
+		}
+	}
+
+}
