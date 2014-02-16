233c4ff6aa7dabac31500550ecb7214f75494d41
Edu <evillarroelcuadrado@gmail.com>
2014-02-16 22:51:55 +0100

    Interfaz añadir producto manualmente

--- KitchApp/src/com/example/kitchapp/Registro.java
+++ KitchApp/src/com/example/kitchapp/Registro.java
@@ -2,7 +2,6 @@ package com.example.kitchapp;
 
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +15,6 @@ public class Registro extends Activity implements OnClickListener {
 	private EditText userName;
 	private EditText password;
 	
-=======
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
- 
-public class Registro extends Activity {
->>>>>>> refs/heads/Rama-Vivi-Android
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +22,6 @@ public class Registro extends Activity {
         setContentView(R.layout.registro);
  
         TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
-<<<<<<< HEAD
         Button buttonRegister = (Button) findViewById(R.id.btnRegistro);
         userName = (EditText) findViewById(R.id.reg_usuario);
         password = (EditText) findViewById(R.id.reg_password);
@@ -59,17 +50,4 @@ public class Registro extends Activity {
 		}
 		
 	}
-=======
- 
-        // Listening to Login Screen link
-        loginScreen.setOnClickListener(new View.OnClickListener() {
- 
-            public void onClick(View arg0) {
-                                // Closing registration screen
-                // Switching to Login Screen/closing register screen
-                finish();
-            }
-        });
-    }
->>>>>>> refs/heads/Rama-Vivi-Android
 }
