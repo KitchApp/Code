0bdcfadf4dd88aa8cd57febd8bcf8e165a989b77
Edu <evillarroelcuadrado@gmail.com>
2014-02-16 22:23:46 +0100

    Interfaz añadir producto manualmente

--- KitchApp/src/com/example/kitchapp/AccesoDespensa.java
+++ KitchApp/src/com/example/kitchapp/AccesoDespensa.java
@@ -1,9 +1,5 @@
 package com.example.kitchapp;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/heads/Rama-Vivi-Android
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -13,21 +9,14 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 public class AccesoDespensa extends Activity implements OnClickListener {
-=======
-public class AccesoDespensa extends Activity implements OnClickListener{
->>>>>>> refs/heads/Rama-Vivi-Android
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_acceso_despensa);
-<<<<<<< HEAD
 		Button b1 = (Button) findViewById(R.id.button1);
 		b1.setOnClickListener(this);
-=======
->>>>>>> refs/heads/Rama-Vivi-Android
 		Toast.makeText(this, "Actividad despensa", Toast.LENGTH_SHORT).show();
 		Button b=(Button)findViewById(R.id.button_add);
 		b.setOnClickListener(this);
@@ -39,16 +28,12 @@ public class AccesoDespensa extends Activity implements OnClickListener{
 		//getMenuInflater().inflate(R.menu.acceso_despensa, menu);
 		return true;
 	}
-<<<<<<< HEAD
 	
 	/*public void mostrarProductos(View view) {
 		Intent intent = new Intent(this,MostrarProductosCategoria.class);
 		startActivity(intent);
 	}*/
 	
-=======
-
->>>>>>> refs/heads/Rama-Vivi-Android
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
@@ -57,7 +42,6 @@ public class AccesoDespensa extends Activity implements OnClickListener{
 			Intent i = new Intent(this,AddDespensa.class);
 			startActivity(i);
 			break;
-<<<<<<< HEAD
 			
 		case R.id.button1:
 			Intent intent = new Intent(this,MostrarProductosCategoria.class);
@@ -69,9 +53,3 @@ public class AccesoDespensa extends Activity implements OnClickListener{
 }
 	
 	
-=======
-		}
-	}
-
-}
->>>>>>> refs/heads/Rama-Vivi-Android
