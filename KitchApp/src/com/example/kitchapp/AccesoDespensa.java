c45e5fb722d60680484b1b82c3c1d4ffd2f8dc97
Mayra <majuma22@gmail.com>
2014-02-28 17:18:03 +0100

    Update AccesoDespensa.java

--- KitchApp/src/com/example/kitchapp/AccesoDespensa.java
+++ KitchApp/src/com/example/kitchapp/AccesoDespensa.java
@@ -3,7 +3,6 @@ package com.example.kitchapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-<<<<<<< HEAD
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -20,13 +19,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
-=======
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
->>>>>>> Rama-Android
+
 
 public class AccesoDespensa extends Activity implements OnClickListener {
 
@@ -37,18 +30,11 @@ public class AccesoDespensa extends Activity implements OnClickListener {
 
 		Button b1 = (Button) findViewById(R.id.button1);
 		b1.setOnClickListener(this);
-<<<<<<< HEAD
-=======
-		Toast.makeText(this, "Actividad despensa", Toast.LENGTH_SHORT).show();
->>>>>>> Rama-Android
 		Button b=(Button)findViewById(R.id.button_add);
 		b.setOnClickListener(this);
 		Button button_add=(Button)findViewById(R.id.button_add);
 		button_add.setOnClickListener(this);
-<<<<<<< HEAD
-=======
-		//Toast.makeText(this, "Actividad despensa", Toast.LENGTH_SHORT).show();
->>>>>>> Rama-Android
+
 	}
 
 	@Override
@@ -58,39 +44,27 @@ public class AccesoDespensa extends Activity implements OnClickListener {
 		return true;
 	}
 	
-<<<<<<< HEAD
-=======
-	/*public void mostrarProductos(View view) {
-		Intent intent = new Intent(this,MostrarProductosCategoria.class);
-		startActivity(intent);
-	}*/
->>>>>>> Rama-Android
+
 	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()){
-<<<<<<< HEAD
-=======
-		/*case R.id.button_add:
-			Intent i = new Intent(this,AddDespensa.class);
-			startActivity(i);
-			break;*/
->>>>>>> Rama-Android
+
 		case R.id.button1:
 			Intent i = new Intent(this,MostrarProductosCategoria.class);
 			startActivity(i);
 			break;
 			
 		case R.id.button_add:
-<<<<<<< HEAD
+
 			alertDialog(v);
 		}
 	}
 	
 	
 	public void alertDialog(View v){
-		final String [] items = new String[] {"Manualmente", "Voz", "Código de barras" };
+		final String [] items = new String[] {"Manualmente", "Voz", "Cï¿½digo de barras" };
 	    final Integer[] icons = new Integer[] {R.drawable.teclado_android, R.drawable.microfono, R.drawable.barras};
 	    ListAdapter adapter = new ItemAdapter(this, items, icons);
 	    
@@ -107,11 +81,5 @@ public class AccesoDespensa extends Activity implements OnClickListener {
 		  Intent intent = new Intent(this,AddManualmente.class);
 		  startActivity(intent);
 	  }
-=======
-			Intent j = new Intent(this,AddDespensa.class);
-			startActivity(j);
-			break;
-		}
-	}
->>>>>>> Rama-Android
+
 }
