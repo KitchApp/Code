bc19c81f8e762b2026442b06370439486d3b88f6
Mayra <majuma22@gmail.com>
2014-04-12 01:17:34 +0200

    Update R.java

--- KitchApp/gen/com/example/kitchapp/R.java
+++ KitchApp/gen/com/example/kitchapp/R.java
@@ -54,30 +54,6 @@ public final class R {
     public static final class drawable {
         public static final int anadir_icono=0x7f020000;
         public static final int barras=0x7f020001;
-<<<<<<< HEAD
-        public static final int boton_redondo_color_aqua=0x7f020002;
-        public static final int boton_redondo_color_celeste=0x7f020003;
-        public static final int boton_redondo_color_navy=0x7f020004;
-        public static final int button_over=0x7f020005;
-        public static final int cabecera_kitchapp=0x7f020006;
-        public static final int close=0x7f020007;
-        public static final int fondo=0x7f020008;
-        public static final int fondo2=0x7f020009;
-        public static final int fondo_boton=0x7f02000a;
-        public static final int fondo_de_pantalla_naranja=0x7f02000b;
-        public static final int fondo_nuevo=0x7f02000c;
-        public static final int fondo_pie_pagina=0x7f02000d;
-        public static final int ic_launcher=0x7f02000e;
-        public static final int images=0x7f02000f;
-        public static final int lupa_sobre_un_robot_androide=0x7f020010;
-        public static final int microfono=0x7f020011;
-        public static final int numberpicker_down=0x7f020012;
-        public static final int numberpicker_up=0x7f020013;
-        public static final int propuestaportadaappis=0x7f020014;
-        public static final int prueba2=0x7f020015;
-        public static final int resumen_de_color_como_fondo=0x7f020016;
-        public static final int teclado_android=0x7f020017;
-=======
         public static final int bebidas=0x7f020002;
         public static final int boton_redondo_color_aqua=0x7f020003;
         public static final int boton_redondo_color_celeste=0x7f020004;
@@ -117,7 +93,7 @@ public final class R {
         public static final int salsas=0x7f020026;
         public static final int teclado_android=0x7f020027;
         public static final int varios=0x7f020028;
->>>>>>> Rama-Mayra_Android
+
     }
     public static final class id {
         public static final int EditTextCantProduct=0x7f0a000f;
