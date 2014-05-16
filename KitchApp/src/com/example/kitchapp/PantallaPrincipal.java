1ec5358e4fc5d44892fa4b5e7783d2ae12c0384f
lorenareyes <lorenareyesp@gmail.com>
2014-05-17 00:56:21 +0200

    Update PantallaPrincipal.java

--- KitchApp/src/com/example/kitchapp/PantallaPrincipal.java
+++ KitchApp/src/com/example/kitchapp/PantallaPrincipal.java
@@ -19,10 +19,7 @@ public class PantallaPrincipal extends Activity implements OnClickListener{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// setting default screen to login.xml
-<<<<<<< HEAD
-		setContentView(R.layout.activity_pantalla_principal);
-=======
-		
+	
 		setContentView(R.layout.activity_pantalla_principal);
 		Button buttonPantry = (Button) findViewById(R.id.button2);
 		buttonPantry.setOnClickListener(this);
@@ -30,24 +27,14 @@ public class PantallaPrincipal extends Activity implements OnClickListener{
 		buttonList.setOnClickListener(this);
 		Button buttonRecipes = (Button) findViewById(R.id.button3);
 		buttonRecipes.setOnClickListener(this);
->>>>>>> Rama-Lorena-Android
 		
 }
 	
-	
-<<<<<<< HEAD
-	public void accederDespensa(View view) {
-		
-		Intent i = new Intent(this, AccesoDespensa.class);
-		//Toast.makeText(this, "Actividad Main ", Toast.LENGTH_SHORT).show();
-		startActivity(i);
-        
-	}
-=======
+
 	public void onClick(View v) {
 		switch(v.getId()) {
 			case R.id.button1:
-				Intent i = new Intent(this, ShoppingLists.class);
+				Intent i = new Intent(this, AccesoDespensa.class);
 				startActivity(i);
 				break;
 			
@@ -59,6 +46,4 @@ public class PantallaPrincipal extends Activity implements OnClickListener{
 		}
 	}
 	
-	
->>>>>>> Rama-Lorena-Android
 }
