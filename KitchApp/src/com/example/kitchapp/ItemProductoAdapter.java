4a28795134295666ef63ffcd05871f516e50b21b
vivianasandagorda <viviycarlos55@gmail.com>
2014-05-22 03:09:07 +0200

    Update ItemProductoAdapter.java

--- KitchApp/src/com/example/kitchapp/ItemProductoAdapter.java
+++ KitchApp/src/com/example/kitchapp/ItemProductoAdapter.java
@@ -11,88 +11,72 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
-<<<<<<< HEAD
-=======
 import android.widget.Toast;
->>>>>>> Rama-Vivi-Android
 
 public class ItemProductoAdapter extends BaseAdapter {
-  protected Activity activity;
-  protected ArrayList<ItemProducto> items;
-         
-  public ItemProductoAdapter(Activity activity, ArrayList<ItemProducto> items) {
-    this.activity = activity;
-    this.items = items;
-  }
- 
-  @Override
-  public int getCount() {
-    return items.size();
-  }
- 
-  @Override
-  public Object getItem(int position) {
-    return items.get(position);
-  }
- 
-  @Override
-  public long getItemId(int position) {
-    return items.get(position).getId();
-  }
- 
-  @Override
-  public View getView(int position, View contentView, ViewGroup parent) {
-    View vi=contentView;
-         
-    if(contentView == null) {
-      LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      vi = inflater.inflate(R.layout.activity_item_listview, null);
-    }
-    
-    CheckBox checkProduct = (CheckBox) vi.findViewById(R.id.checkBox_Product);
-    checkProduct.setOnClickListener(new OnClickListener() {
-    	@Override
-    	public void onClick(View v) {
-    		ItemProducto data = (ItemProducto) v.getTag();
-    		data.setSelected(((CheckBox) v).isChecked());
-    	}
-    });
-<<<<<<< HEAD
-
-    ItemProducto item = items.get(position);
-         
-=======
+      protected Activity activity;
+      protected ArrayList<ItemProducto> items;
              
-    ItemProducto item = items.get(position);
-         
-    /*ImageView image = (ImageView) vi.findViewById(R.id.imagen);
-    int imageResource = activity.getResources().getIdentifier(item.getRutaImagen(), null, activity.getPackageName());
-    image.setImageDrawable(activity.getResources().getDrawable(imageResource));*/
-         
->>>>>>> Rama-Vivi-Android
-    TextView nombre = (TextView) vi.findViewById(R.id.textView_Item_Name);
-    nombre.setText(item.getNombre());
-         
-    TextView cantidad = (TextView) vi.findViewById(R.id.textView_Item_Cant);
-<<<<<<< HEAD
+      public ItemProductoAdapter(Activity activity, ArrayList<ItemProducto> items) {
+        this.activity = activity;
+        this.items = items;
+      }
+     
+      @Override
+      public int getCount() {
+        return items.size();
+      }
+     
+      @Override
+      public Object getItem(int position) {
+        return items.get(position);
+      }
+     
+      @Override
+      public long getItemId(int position) {
+        return items.get(position).getId();
+      }
+     
+      @Override
+      public View getView(int position, View contentView, ViewGroup parent) {
+        View vi=contentView;
+             
+        if(contentView == null) {
+          LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+          vi = inflater.inflate(R.layout.activity_item_listview, null);
+        }
+        
+        CheckBox checkProduct = (CheckBox) vi.findViewById(R.id.checkBox_Product);
+        checkProduct.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		ItemProducto data = (ItemProducto) v.getTag();
+        		data.setSelected(((CheckBox) v).isChecked());
+        	}
+        });
+               
+        ItemProducto item = items.get(position);
+             
+        /*ImageView image = (ImageView) vi.findViewById(R.id.imagen);
+        int imageResource = activity.getResources().getIdentifier(item.getRutaImagen(), null, activity.getPackageName());
+        image.setImageDrawable(activity.getResources().getDrawable(imageResource));*/
+             
+        TextView nombre = (TextView) vi.findViewById(R.id.textView_Item_Name);
+        nombre.setText(item.getNombre());
+             
+        TextView cantidad = (TextView) vi.findViewById(R.id.textView_Item_Cant);
+    
+        if (item.getCantidad() == 0) {
+        	cantidad.setText("");
+        }
+        else {
+        	cantidad.setText(item.getCantidad() + "");
+        }
+        
+        checkProduct.setChecked(item.isSelected());
+        checkProduct.setTag(item);
+     
+        return vi;
+      }
 
-=======
-    
->>>>>>> Rama-Vivi-Android
-    if (item.getCantidad() == 0) {
-    	cantidad.setText("");
-    }
-    else {
-    	cantidad.setText(item.getCantidad() + "");
-    }
-    
-    checkProduct.setChecked(item.isSelected());
-    checkProduct.setTag(item);
- 
-    return vi;
-  }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> Rama-Vivi-Android
