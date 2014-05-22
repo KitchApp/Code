8ed8995103a10922ed7f26d58246c02aa5454944
vivianasandagorda <viviycarlos55@gmail.com>
2014-05-22 03:07:05 +0200

    Update ItemProducto.java

--- KitchApp/src/com/example/kitchapp/ItemProducto.java
+++ KitchApp/src/com/example/kitchapp/ItemProducto.java
@@ -1,124 +1,76 @@
 package com.example.kitchapp;
 
 public class ItemProducto {
-  protected long id;
-  protected int cantidad;
-  protected String nombre;
-  protected boolean selected;
-  protected int category;
-  protected String barCode;
-  
-  public ItemProducto() {
-    this.nombre = "";
-    //this.tipo = "";
-    this.cantidad = 0;
-    this.selected = false;
-  }
-<<<<<<< HEAD
-  
-  public ItemProducto(int id, String nombre, int cantidad) {
-	    this.id = id;
-	    this.nombre = nombre;
-	    //this.tipo = tipo;
-	    this.cantidad = cantidad;
-	    //this.barCode=barCode;
-  }
-=======
-     
-  //public ItemProducto(long id, String nombre, int cantidad, String tipo) {
-  public ItemProducto(int id, String nombre, int cantidad,int category, boolean selected) {
-	    this.id = id;
-	    this.nombre = nombre;
-	    //this.tipo = tipo;
-	    this.category = category;
-	    this.cantidad = cantidad;
-	    this.selected = selected;
-	    //this.barCode=barCode;
+	  protected long id;
+	  protected int cantidad;
+	  protected String nombre;
+	  protected boolean selected;
+	  protected int category;
+	  protected String barCode;
+	  
+	  public ItemProducto() {
+	    this.nombre = "";
+	    //this.tipo = "";
+	    this.cantidad = 0;
+	    this.selected = false;
 	  }
->>>>>>> Rama-Vivi-Android
-     
-  //public ItemProducto(long id, String nombre, int cantidad, String tipo) {
-  public ItemProducto(int id, String nombre, int cantidad,int category, boolean selected) {
+	     
+	  //public ItemProducto(long id, String nombre, int cantidad, String tipo) {
+	  public ItemProducto(int id, String nombre, int cantidad,int category, boolean selected) {
+		    this.id = id;
+		    this.nombre = nombre;
+		    //this.tipo = tipo;
+		    this.category = category;
+		    this.cantidad = cantidad;
+		    this.selected = selected;
+		    //this.barCode=barCode;
+		  }
+	     
+	  /*public Item(long id, String nombre, String tipo, String rutaImagen) {
 	    this.id = id;
 	    this.nombre = nombre;
-	    //this.tipo = tipo;
-	    this.category = category;
-	    this.cantidad = cantidad;
-	    this.selected = selected;
-	    //this.barCode=barCode;
+	    this.tipo = tipo;
+	    this.rutaImagen = rutaImagen;
+	  }*/
+	     
+	  public long getId() {
+	    return id;
 	  }
-     
-  /*public Item(long id, String nombre, String tipo, String rutaImagen) {
-    this.id = id;
-    this.nombre = nombre;
-    this.tipo = tipo;
-    this.rutaImagen = rutaImagen;
-  }*/
-     
-  public long getId() {
-    return id;
-  }
-     
-  public void setId(long id) {
-    this.id = id;
-  }
-     
-  
-  public String getNombre() {
-    return nombre;
-  }
-     
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-  
-  public int getCantidad() {
-	  return cantidad;
-  }
-  
-  public void setCantidad(int cantidad) {
-	  this.cantidad = cantidad;
-  }
-<<<<<<< HEAD
-
-  public boolean isSelected() {
-	  return selected;
-=======
-  
-  public boolean isSelected() {
-	  return selected;
-  }
-  
-  public void setSelected(boolean selected) {
-	  this.selected = selected;
-  }
-  
-  public int getCategory() {
-	  return category;
-  }
-  
-  public void setCategory(int category) {
-	  this.category = category;
-  }
-     
- /* public String getTipo() {
-    return tipo;
->>>>>>> Rama-Vivi-Android
-  }
-  
-<<<<<<< HEAD
-  public void setSelected(boolean selected) {
-	  this.selected = selected;
-  }
-  
-  public int getCategory() {
-	  return category;
-  }
-  
-  public void setCategory(int category) {
-	  this.category = category;
-  }
-
-=======
->>>>>>> Rama-Vivi-Android
+	     
+	  public void setId(long id) {
+	    this.id = id;
+	  }
+	     
+	  
+	  public String getNombre() {
+	    return nombre;
+	  }
+	     
+	  public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	  }
+	  
+	  public int getCantidad() {
+		  return cantidad;
+	  }
+	  
+	  public void setCantidad(int cantidad) {
+		  this.cantidad = cantidad;
+	  }
+	
+	  public boolean isSelected() {
+		  return selected;
+	  }
+	  
+	  public void setSelected(boolean selected) {
+		  this.selected = selected;
+	  }
+	  
+	  public int getCategory() {
+		  return category;
+	  }
+	  
+	  public void setCategory(int category) {
+		  this.category = category;
+	  }
 }
