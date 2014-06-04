0b5be72f5c67a7c1c8050a3a8de086316fd9ca8e
vivianasandagorda <viviycarlos55@gmail.com>
2014-06-04 08:14:20 +0200

    Update ItemReceta.java

--- KitchApp/src/com/example/kitchapp/ItemReceta.java
+++ KitchApp/src/com/example/kitchapp/ItemReceta.java
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-package com.example.kitchapp;
-
-public class ItemReceta {
-
-	protected long id;
-	protected String nombre;
-	//protected String rutaImagen;
-	
-	public ItemReceta() {
-		this.nombre = "";		    
-	}
-		     
-	public ItemReceta(int id,String nombre) {
-		this.id=id;
-	    	this.nombre = nombre;	
-	}
-		 		     
-	public long getId() {
-	    	return id;
-	}
-		     
-	public void setId(long id) {
-	    	this.id = id;
-	}
-		     
-	/*public String getRutaImagen() {
-	    return rutaImagen;
-	}
-		     
-	public void setRutaImagen(String rutaImagen) {
-	    this.rutaImagen = rutaImagen;
-	}*/
-		     
-	public String getNombre() {
-	    	return nombre;
-	}
-		     
-	public void setNombre(String nombre) {
-	    this.nombre = nombre;
-	}
-		  
-}
-=======
 package com.example.kitchapp;
 
 public class ItemReceta {
@@ -90,4 +46,4 @@ public class ItemReceta {
 		  
 			  
 }
->>>>>>> Rama-Vivi-Android
+
