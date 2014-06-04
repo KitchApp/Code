0c7ac9e6130473ddd2e1a42f19de8b3e5af4d0e6
vivianasandagorda <viviycarlos55@gmail.com>
2014-06-04 08:19:44 +0200

    Update ShoppingListItem.java

--- KitchApp/src/com/example/kitchapp/ShoppingListItem.java
+++ KitchApp/src/com/example/kitchapp/ShoppingListItem.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package com.example.kitchapp;
-
-public class ShoppingListItem {
-	
-	private String listName;
-	private boolean checked;
-	
-	public ShoppingListItem(String name){
-		listName = name;
-	}
-	
-	public String getListName() {
-		return listName;
-	}
-	
-	public void setListName(String listName) {
-		this.listName = listName;
-	}
-	
-	public boolean isChecked() {
-		return checked;
-	}
-	
-	public void setChecked(boolean checked) {
-		this.checked = checked;
-	}
-
-	
-}
-=======
 package com.example.kitchapp;
 
 public class ShoppingListItem {
@@ -59,4 +28,4 @@ public class ShoppingListItem {
 
 	
 }
->>>>>>> Rama-Vivi-Android
+
