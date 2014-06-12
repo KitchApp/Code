55eafba395847609b0537bbf4f9cdae243b66b28
Mayra <majuma22@gmail.com>
2014-06-12 12:03:32 +0200

    Update Interface.java

--- KitchApp/src/com/example/kitchapp/Interface.java
+++ KitchApp/src/com/example/kitchapp/Interface.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.kitchapp;
 
 import java.util.ArrayList;
@@ -7,13 +7,3 @@ public interface Interface {
 	ArrayList<String> processFinish(ArrayList<String>  output);
 }
 
-=======
-package com.example.kitchapp;
-
-import java.util.ArrayList;
-
-public interface Interface {
-	ArrayList<String> processFinish(ArrayList<String>  output);
-}
-
->>>>>>> Rama-Mayra_Android
