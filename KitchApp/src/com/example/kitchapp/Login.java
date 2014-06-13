e48035ac01a11a7612af051370e5cf112aab4180
lorenareyes <lorenareyesp@gmail.com>
2014-06-13 19:55:17 +0200

    Update Login.java

--- KitchApp/src/com/example/kitchapp/Login.java
+++ KitchApp/src/com/example/kitchapp/Login.java
@@ -31,29 +31,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends Activity {
-<<<<<<< HEAD
-	
-=======
 
-	private EditText userName;
-	private EditText password;
->>>>>>> Rama-Lorena-Android
 	Handler_Sqlite helper = new Handler_Sqlite(this);
 	Boolean registrado = false;
-
 	public String session_name;
-<<<<<<< HEAD
-    public String session_id;
-    public String encryptedPassword="";
-    private EditText userName;
+    	public String session_id;
+    	public String encryptedPassword="";
+    	private EditText userName;
 	private EditText password;
-	
-	
-=======
-	public String session_id;
-	public String encryptedPassword = "";
 
->>>>>>> Rama-Lorena-Android
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
@@ -73,8 +59,6 @@ public class Login extends Activity {
 
 		setContentView(R.layout.activity_login);
 
-<<<<<<< HEAD
-		
 		userName = (EditText)findViewById(R.id.editTextuserName);
 		password = (EditText)findViewById(R.id.editTextPassword);
 
@@ -83,24 +67,13 @@ public class Login extends Activity {
 		
     	
 	}
-=======
-		userName = (EditText) findViewById(R.id.editTextuserName);
-		password = (EditText) findViewById(R.id.editTextPassword);
-
-		TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
-		Button b1 = (Button) findViewById(R.id.btnLogin);
->>>>>>> Rama-Lorena-Android
-
 	}
 
 	public void intento_logueo(View view) {
-<<<<<<< HEAD
-		
-=======
+
 		String name = userName.getText().toString();
 		String key = password.getText().toString();
 
->>>>>>> Rama-Lorena-Android
 		new HttpAsyncTask().execute();
 	}
 
@@ -133,123 +106,6 @@ public class Login extends Activity {
 	}
 
 	private class HttpAsyncTask extends AsyncTask<String, Integer, Integer> {
-<<<<<<< HEAD
-		
-
-	
-        @Override
-        protected Integer doInBackground(String... urls) {
-        	
-        	
-        	HttpClient httpclient = new DefaultHttpClient();
-
-            //set the remote endpoint URL
-            HttpPost httppost = new HttpPost("http://www.kitchapp.es/json/user/login");
-            
-            try {
-
-                //get the UI elements for username and password
-            	EditText username= (EditText) findViewById(R.id.editTextuserName);
-                EditText password= (EditText) findViewById(R.id.editTextPassword);
-
-                JSONObject json = new JSONObject();
-                //extract the username and password from UI elements and create a JSON object
-                json.put("username", username.getText().toString().trim());
-                json.put("password", password.getText().toString().trim());
-
-                //add serialised JSON object into POST request
-                StringEntity se = new StringEntity(json.toString());
-                //set request content type
-                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-                httppost.setEntity(se);
-
-                //send the POST request
-                HttpResponse response = httpclient.execute(httppost);
-
-                //read the response from Services endpoint
-                String jsonResponse = EntityUtils.toString(response.getEntity());
-
-                JSONObject jsonObject = new JSONObject(jsonResponse);
-                //read the session information
-                session_name=jsonObject.getString("session_name");
-                session_id=jsonObject.getString("sessid");
-                if (jsonObject!=null)
-                	registrado=true;
-                return 0;
-
-            }catch (Exception e) {
-                Log.v("Error adding article", e.getMessage());
-            }
-
-            return 0;
-        }
-
-       /** onPostExecute displays the results of the AsyncTask.**/
-       @Override
-        protected void onPostExecute(Integer result) {
-            if(registrado){
-        		SharedPreferences settings = getSharedPreferences(PantallaTransicion.PREFS_NAME, 0);
-				SharedPreferences.Editor editor = settings.edit();
-				
-				editor.putBoolean("hasLoggedIn", true);
-				editor.commit();
-
-    			Intent intent = new Intent(Login.this,PantallaTransicion.class);
-    	    	startActivity(intent);
-    			finish();
-    		}
-    		else
-    			errorLogging();
-            
-            
-       }
-    }
-
-
-	
-	public static String httpGetData(String mURL) {
-        InputStream inputStream = null;
-	        String result = "";
-	        try {
-	 
-	            // create HttpClient
-	            HttpClient httpclient = new DefaultHttpClient();
-	 
-	            // make GET request to the given URL
-	            HttpResponse httpResponse = httpclient.execute(new HttpGet(mURL));
-	 
-	            // receive response as inputStream
-	            inputStream = httpResponse.getEntity().getContent();
-	 
-	            // convert inputstream to string
-	            if(inputStream != null)
-	                result = convertInputStreamToString(inputStream);
-	            else
-	                result = "Did not work!";
-	 
-	        } catch (Exception e) {
-	            Log.d("InputStream", e.getLocalizedMessage());
-	        }
-	 
-	        return result;
-    
-    }  
-	
-	 
-	 private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-	        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-	        String line = "";
-	        String result = "";
-	        while((line = bufferedReader.readLine()) != null)
-	            result += line;
-	 
-	        inputStream.close();
-	        return result;
-	 
-	  }
-	
-
-=======
 
 		@Override
 		protected Integer doInBackground(String... urls) {
@@ -366,5 +222,4 @@ public class Login extends Activity {
 	public void onBackPressed() {
 		super.finish();
 	}
->>>>>>> Rama-Lorena-Android
 }
